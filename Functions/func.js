99a0f491b7ef4e4173f1c11e579d9ccad35cfa94
Saima02 <ansarisaimaa02@gmail.com>
2025-09-15 18:13:44 +0530

    Added functions folder

---
+++ Functions/func.js
@@ -0,0 +1,23 @@
+function Sum(a,b) {
+    const Sum = a - b;
+    console.log("result", Sum);
+}
+
+const a = 12;
+const b = 5;
+Sum(a,b);
+
+const c = 12;
+const d = 45;
+Sum(c,d);
+
+const e = 2;
+const f = 5;
+Sum(e,f);
+
+function loginuserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginuserMessage("john"));
+
+ 
