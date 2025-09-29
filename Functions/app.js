9c342c118a4e7689b8b814b3d302398828e9a512
Saima02 <ansarisaimaa02@gmail.com>
2025-09-29 11:22:06 +0530

    Added app.js

---
+++ Functions/app.js
@@ -0,0 +1,11 @@
+function multiplegreet(func, count) {
+    for(let i=1; i<=count; i++) {
+        func();
+    }
+}
+
+let greet = function() {
+    console.log("hello");
+}
+
+multiplegreet(greet, 5)
